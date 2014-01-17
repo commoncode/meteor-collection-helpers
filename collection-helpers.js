512f819f76dba9c806d57213ad299d1cecf78152
David Burles <dburles@gmail.com>
2014-01-18 09:30:12 +1100

    slight refactor

--- collection-helpers.js
+++ collection-helpers.js
@@ -6,12 +6,12 @@ Meteor.Collection.prototype.helpers = function(helpers) {
   if (! Document[self._name])
     Document[self._name] = function(doc) { return _.extend(this, doc); };
 
+  if (! self._transform)
+    self._transform = function(doc) { return new Document[self._name](doc); };
+
   _.each(helpers, function(helper, key) {
     Document[self._name].prototype[key] = helper;
   });
-
-  if (! self._transform)
-    self._transform = function(doc) { return new Document[self._name](doc); };
 };
 
 Meteor.Collection.helpers = function(helpers, collections) {
@@ -20,16 +20,16 @@ Meteor.Collection.helpers = function(helpers, collections) {
 
   if (! _.isArray(collections))
     collections = [collections];
+  
+  _.each(collections, function(collection) {
+    if (! Document[collection._name])
+      Document[collection._name] = function(doc) { return _.extend(this, doc); };
 
-  _.each(helpers, function(helper, key) {
-    _.each(collections, function(collection) {
-      if (! Document[collection._name])
-        Document[collection._name] = function(doc) { return _.extend(this, doc); };
+    if (! collection._transform)
+      collection._transform = function(doc) { return new Document[collection._name](doc); };
 
+    _.each(helpers, function(helper, key) {
       Document[collection._name].prototype[key] = helper;
-
-      if (! collection._transform)
-        collection._transform = function(doc) { return new Document[collection._name](doc); };
     });
   });
 };
