25ca58adc7a641f22916c7ea2e70ce7b7753776f
David Burles <dburles@gmail.com>
2014-01-17 23:20:47 +1100

    allow common helpers to be defined
    #3

--- collection-helpers.js
+++ collection-helpers.js
@@ -1,9 +1,35 @@
 var Document = {};
+
 Meteor.Collection.prototype.helpers = function(helpers) {
   var self = this;
-  if (! Document[self._name]) Document[self._name] = function(doc) { return _.extend(this, doc); };
+
+  if (! Document[self._name])
+    Document[self._name] = function(doc) { return _.extend(this, doc); };
+
   _.each(helpers, function(helper, key) {
     Document[self._name].prototype[key] = helper;
   });
-  if (! self._transform) this._transform = function(doc) { return new Document[self._name](doc); };
+
+  if (! self._transform)
+    self._transform = function(doc) { return new Document[self._name](doc); };
+};
+
+Meteor.Collection.helpers = function(helpers, collections) {
+  if (_.isUndefined(collections))
+    return;
+
+  if (! _.isArray(collections))
+    collections = [collections];
+
+  _.each(helpers, function(helper, key) {
+    _.each(collections, function(collection) {
+      if (! Document[collection._name])
+        Document[collection._name] = function(doc) { return _.extend(this, doc); };
+
+      Document[collection._name].prototype[key] = helper;
+
+      if (! collection._transform)
+        collection._transform = function(doc) { return new Document[collection._name](doc); };
+    });
+  });
 };
