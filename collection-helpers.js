35a6e5597ca7c764c057cf322ace372c63c82301
Daryl Antony <daryl@commoncode.com.au>
2014-06-05 00:50:01 +1000

    Cheeky Collection reference on self.

--- collection-helpers.js
+++ collection-helpers.js
@@ -3,7 +3,12 @@ Meteor.Collection.prototype.helpers = function(helpers) {
   var self = this;
   if (! Document[self._name]) Document[self._name] = function(doc) { return _.extend(this, doc); };
   if (! self._transform) self._transform = function(doc) { return new Document[self._name](doc); };
+  // A cheeky reference to the Collection is placed on the Document to give it more
+  // introspective powers.  Note, the data type is lost if passed through to a Meteor method
+  // or any other such magix.
+  Document[self._name].prototype['Collection'] = self;
   _.each(helpers, function(helper, key) {
     Document[self._name].prototype[key] = helper;
   });
+
 };
